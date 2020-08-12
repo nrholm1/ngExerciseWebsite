040783dd9be88dd6eaad788c13189a4c41bea5a3
nrholm1 <nielsraunkjaer@gmail.com>
2020-08-12 21:58:59 +0200

    Created async GET methods for SetService

--- src/app/_services/set.service.ts
+++ src/app/_services/set.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { set } from '../_models/set';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SetService {
+  apiUrl: string = environment.api;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  async getSets() {
+    return await this.http.get<set[]>(this.apiUrl).toPromise();
+  }
+
+  async getSetById(id: number) {
+    return await this.http.get<set>(this.apiUrl + "/" + id).toPromise();
+  }
 }
